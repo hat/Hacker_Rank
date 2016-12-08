a8e54d82965c4017b00d2645266a958b36c05359
Tony <phillyeagles60@gmail.com>
2016-12-08 12:33:19 -0800

    Day 8 - Dictionaries and Maps
    Word other than on test 1 because of the time it takes.

---
+++ dictionaries_maps.cpp
@@ -0,0 +1,39 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+int main() {
+    int                 num_entries;
+    string              name;
+    string              search;
+    int                 number;
+    map<string, int>    phone_book; // Used to store name and number
+   
+    //get the number going to be entered from stdin 
+    cin >> num_entries;
+    cin.ignore();
+
+    //add each name and number to phone_book
+    for (int i = 0; i < num_entries; i++)
+    {
+        cin >> name;
+        cin >> number;
+        cin.ignore();
+        phone_book[name] = number;
+    }
+    //search phone_book for entered name
+    for (int j = 0; j < num_entries; j++)
+    {
+        getline(cin, search);
+        if (phone_book.count(search) > 0)
+            cout << search << "=" << phone_book[search] << endl;
+        else
+            cout << "Not found" << endl;
+    }
+    return 0;
+}
